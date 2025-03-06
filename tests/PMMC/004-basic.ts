56f6c36b69cb34522c7ca0e36c85a2113ddeefd9
Stevan Little <stevan.little@gmail.com>
2025-03-06 21:39:55 +0100

    test

--- tests/PMMC/004-basic.ts
+++ tests/PMMC/004-basic.ts
@@ -5,22 +5,20 @@ import { Sinks }   from '../../src/PMMC/Sinks';
 
 async function Test003 () {
     //let src = new Sources.REPL();
-    //let src = new Sources.FromArray(
-    //    [
-    //        `
-    //        : plusTen     10 + ;
-    //        : timesTwenty 20 * ;
-    //        1 plusTen timesTwenty 3 -
-    //        `,
-    //    ]
-    //);
+    let src = new Sources.FromArray(
+        [
+            `
+            : plusTen     10 + ;
+            : timesTwenty 20 * ;
+            1 plusTen timesTwenty 3 -
+            `,
+        ]
+    );
 
-    (new Sinks.Console(
-        (new Images.BaseImage()).compile().run(
-            new Sources.REPL()
-        ))
-    ).run()
-        .then((i) => console.log(i));
+    let img  = new Images.BaseImage();
+    let sink = new Sinks.Console(img.compile().run(src));
+
+    sink.run().then((i) => console.log(i));
 }
 
 Test003();
